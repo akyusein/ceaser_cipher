e501df883a34c78bbdcdbd6908d2ffbd7c8e603b
akyusein <155920278+akyusein@users.noreply.github.com>
2024-01-07 21:30:13 +0000

    Add files via upload

---
+++ ceaser_cipher.py
@@ -0,0 +1,42 @@
+from art import logo
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+print(logo)
+
+def caeser(start_text, shift_amount, cipher_direcion):
+    end_text = ""
+    if cipher_direcion == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+    print(f"Your {cipher_direcion}d message is {end_text}")
+
+
+def main():
+    print("Welcome to this ancient encryption programme!")
+    while True:
+        direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+        text = input("Type your message:\n").lower()
+        shift = int(input("Type the shift number:\n"))
+        shift = shift % 26
+        caeser(text, shift, direction)
+        play_again = input("Do you want to go again? Type 'yes' or 'no': ").lower()
+        if play_again != "yes":
+            print("Goodbye!")
+            break
+    
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
